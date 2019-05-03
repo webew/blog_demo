4eb19a49d3ef85a37598a82b031ad6e3dfb6afb2
webew <fillion.bertran@gmail.com>
2019-05-03 16:44:43 +0200

    ajout join

--- index.php
+++ index.php
@@ -9,18 +9,19 @@
 <body>
     <?php
         $pdo = new PDO("mysql:host=localhost;dbname=blog1;charset=utf8;port=3306", "root", "");
-        $req = $pdo->query("SELECT * FROM article");
+        $req = $pdo->query("SELECT * FROM article JOIN categorie c ON article.id_categorie = c.id_categorie");
         $articles = $req->fetchAll(PDO::FETCH_OBJ);
         var_dump($articles);
 
-        for($i = 1; $i<=5; $i++) {
+        foreach($articles as $article) :
     ?>
         <div>
-            <h2>Titre 1</h2>
-            <p>Contenu </p>
+            <h2><?php echo $article->titre_article ?></h2>
+            <p><?= substr($article->contenu_article,0,50) ?></p>
+            <p><?= $article->nom_categorie ?></p>
         </div>
     <?php
-        }
+        endforeach;
     ?>
 </body>
 </html>
