a3b71aa7212c0aeb8631e1ab9b2ea6b2525e2511
webew <fillion.bertrand@gmail.com>
2019-05-03 18:43:48 +0200

    ajout commentaires

--- index.php
+++ index.php
@@ -14,14 +14,14 @@
         $req = $pdo->query("SELECT * FROM article JOIN categorie c ON article.id_categorie = c.id_categorie");
         //récupération des données sous forme de tableau d'objets
         $articles = $req->fetchAll(PDO::FETCH_OBJ);
-        var_dump($articles);
-
+        //var_dump($articles);
+        //affichage des articles
         foreach($articles as $article) :
     ?>
         <div>
-            <h2><?php echo $article->titre_article ?></h2>
-            <p><?= substr($article->contenu_article,0,50) ?></p>
-            <p><?= $article->nom_categorie ?></p>
+            <h2><?= $article->titre_article ?></h2><!--le titre de l'article-->
+            <p><?= substr($article->contenu_article,0,50) ?></p><!--les 50 premiers caractères de l'article-->
+            <p><?= $article->nom_categorie ?></p><!--la catégorie de l'article-->
         </div>
     <?php
         endforeach;
