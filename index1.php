f9a55a66ddf229b91e6fc232b4fe5ad49ff0f80b
webew <fillion.bertran@gmail.com>
2019-05-03 14:02:33 +0200

    first commit

---
+++ index1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        for($i = 1; $i<=5; $i++) :
+    ?>
+        <div>
+            <h2>Titre 1</h2>
+            <p>Contenu kjsdhfjksd</p>
+        </div>
+    <?php
+        endfor;
+    ?>
+</body>
+</html>
